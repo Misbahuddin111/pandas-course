826b40794012461929e36f30487f7cd1d252c738
Misbahuddin <misbahdin63@gmail.com>
2025-06-11 17:05:25 +0500

    pandas

--- 17 session dataframe.py
+++ 17 session dataframe.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 data = [
@@ -28,36 +29,41 @@ ipl = pd.read_csv("ipl-matches.csv")
 # dataFrame methods and attributtes
 
 # shape (shape tell you the no of rows and columns in a file)
-print(movies.shape)
-print(ipl.shape)
+#print(movies.shape)
+#print(ipl.shape)
 
 # dtypes (data type of every column)
 
-print("data type :", movies.dtypes)
+# print("data type :", movies.dtypes)
 
 # index ( show the total no of index)
-print("index",ipl.index)
+# print("index",ipl.index)
 
-#columns (show the total no of columns )
-print("columns :",movies.columns)
+# columns (show the total no of columns )
+# print("columns :",movies.columns)
+
 # values ( show all values in a file)
-print('values :', ipl.values)
+# print('values :', ipl.values)
 
 # head ( print first five elements)
+print(f"first five movies name\n:{movies.head()}")
 
 # tail (print last five elements)
+print(f"last five movies names {ipl.tail()}")
 
 # info (give informTION)
+print(f"information {movies.info()}")
 
 # desceibe ( give mathmatical summary)
+print(f"describe mathmatical overview {ipl.describe()}")
 
 # isnull ( show null values in columns)
+print(f"this functions tells there are how many null values {ipl.isnull()}")
 
 # duplicated (show duplicated values in rows)
+print("duplicates values", ipl.duplicated().sum())
 
 # rename (change the name of heading)
 
-
-
-#  MATH FUNCTIONS
-
+data_frame.rename(columns={"name": "full name","roll no" : "class no"},inplace=True)
+print(data_frame)
