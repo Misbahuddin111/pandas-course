f18c0c185912ae5e33cc69333d9f9033d68cd083
Misbahuddin <misbahdin63@gmail.com>
2025-06-22 18:47:07 +0500

    pandas

--- 20 session merginig , joining.py
+++ 20 session merginig , joining.py
@@ -7,7 +7,7 @@ nov = pd.read_csv("reg-month1.csv")
 dec = pd.read_csv("reg-month2.csv")
 
 delivery = pd.read_csv("deliveries.csv")
-match = pd.read_csv("ipl-matches.csv")
+match = pd.read_csv("matches.csv")
 
 # pd.concat
 # df.concat
@@ -18,3 +18,11 @@ match = pd.read_csv("ipl-matches.csv")
 
 print(pd.concat([nov,dec],ignore_index=True))
 print(pd.concat([nov,dec],keys=["nov","dec"]))
+
+multi = pd.concat([nov,dec],keys=["nov","dec"])
+print(multi.loc[("dec")])
+# fetching data 
+print(multi.loc[("nov",3)])
+
+# concat dataframes horizontally
+print(pd.concat([nov,dec],axis=1,keys=["nov","dec"]))
