b76025262107c68ff317d7777382ec6f0e917151
Misbahuddin <misbahdin63@gmail.com>
2025-06-23 20:14:23 +0500

    pandas

--- 20 session tasks.py
+++ 20 session tasks.py
@@ -10,3 +10,31 @@ students = pd.read_csv('students.csv')
 regs = pd.concat([nov,dec],ignore_index=True)
 total_revenue = regs.merge(courses,how="inner",on='course_id')["price"].sum()
 print(total_revenue)
+
+
+# 2. find month by month revenue
+temp_df = pd.concat([nov,dec],keys=["nov","dec"]).reset_index()
+temp_df.merge(courses,how="inner",on="course_id").groupby("level_0")["price"].sum()
+
+
+# 3. Print the registration table
+# cols -> name -> course -> price
+regs.merge(students,how="inner",on="student_id").merge(courses,how="inner", on="course_id")[["name","course_name","price"]]
+
+# 4. Plot bar chart for revenue/course
+regs.merge(courses,how="inner",on="course_id").groupby("course_name")["price"].sum().plot(kind="bar")
+
+
+# 5. find students who enrolled in both the months
+common_student_id = np.intersect1d(nov['student_id'],dec['student_id'])
+print(common_student_id)
+
+
+# 6. find top 3 students who did most number enrollments
+regs.merge(students,on='student_id').groupby(['student_id','name'])['name'].count().sort_values(ascending=False).head(3)
+
+
+# 7. find top 3 students who spent most amount of money on courses
+regs.merge(students,on='student_id').merge(courses,on='course_id').groupby(['student_id','name'])['price'].sum().sort_values(ascending=False).head(3)
+
+
