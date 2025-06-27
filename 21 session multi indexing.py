b07461848e19f9313d0d9cc9585be1c68e1dc397
Misbahuddin <misbahdin63@gmail.com>
2025-06-27 23:52:02 +0500

    pandas

--- 21 session multi indexing.py
+++ 21 session multi indexing.py
@@ -205,3 +205,28 @@ pivot1 = pd.pivot_table(df,
                         aggfunc=['sum','mean','max'])
 print("\nPivot Table (Average Sales by City and Category):")
 print(pivot1)
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+'''
+
+total_bill	tip	sex	smoker	day	time	size
+0	16.99	1.01	Female	No	Sun	Dinner	2
+1	10.34	1.66	Male	No	Sun	Dinner	3
+2	21.01	3.50	Male	No	Sun	Dinner	3
+3	23.68	3.31	Male	No	Sun	Dinner	2
+4	24.59	3.61	Female	No	Sun	Dinner	4
+
+'''
+
+df = sns.load_dataset('tips')
+df.head()
+
+# aggfunc
+df.pivot_table(index='sex',columns='smoker',values='total_bill',aggfunc='std')
+# margins
+df.pivot_table(index='sex',columns='smoker',values='total_bill',aggfunc='sum',margins=True)
---
+++ 22 session vectorized string.py
@@ -0,0 +1,82 @@
+import pandas as pd
+
+data = {
+    'review_id': [1, 2, 3, 4],
+    'customer_name': ['John Doe', 'MARY SMITH', 'alice brown', 'Bob Jones'],
+    'review_text': [
+        'Great product!!!', 
+     'Not bad, but could be better...', 
+        'AWESOME ITEM', 
+        'poor quality :('
+    ],
+    'product_category': ['Electronics', 'Clothing', 'Electronics', 'Home']
+}
+df = pd.DataFrame(data)
+print(df)
+# change lower() and upper
+print(df["customer_name"].str.lower())
+
+'''
+Real-World Application: In a customer database, 
+names may be entered in mixed cases (e.g., "JOHN", "john", "John"). 
+Converting to lowercase ensures consistency for matching or grouping.
+
+'''
+
+# . str.strip()  Clean up messy text data, such as reviews with extra spaces.
+df["review_text"]=df["review_text"].str.strip()
+
+print(df["review_text"])
+
+
+''''
+Real-World Application: In user-submitted data (e.g., survey responses), extra spaces are common.
+Stripping them ensures clean text for analysis or natural language processing (NLP).
+'''
+
+#str.replace()  Fixes unwanted characters or standardizes words (e.g., change "poor" to "bad").
+
+import pandas as pd
+
+data = {
+    'customer_name': ['John Doe', 'MARY SMITH', 'alice brown', 'Bob Jones'],
+    'review': ['Great product!!!', 'Not bad, but ok...', 'AWESOME BUY', 'poor quality :(']
+}
+df = pd.DataFrame(data)
+print(df)
+
+# Replace "!!!" with "!" in reviews
+df['review'] = df['review'].str.replace('!!!', '!', regex=False)
+print(df['review'])
+
+
+# str.contains() Helps find rows with specific keywords, like positive or negative feedback. 
+
+# Find reviews with the word "great"
+has_great = df['review'].str.contains('great', case=False, na=False)
+print(df[has_great])
+
+'''
+Real-Life Use: In customer reviews, you might search for words like "awesome" or
+"terrible" to identify happy or unhappy customers for further action
+'''
+
+# str.split() Breaks text into pieces based on a separator (like a space or comma).
+
+# Split customer names into first and last names
+df[['first_name', 'last_name']] = df['customer_name'].str.split(' ', expand=True)
+print(df[['customer_name', 'first_name', 'last_name']])
+
+'''
+Real-Life Use: In an employee database, 
+splitting names into first and last names helps with sorting or sending personalized emails.
+'''
+
+# str.len() Counts how many characters are in each text.
+
+# Count characters in each review
+df['review_length'] = df['review'].str.len()
+print(df[['review', 'review_length']])
+
+
+
